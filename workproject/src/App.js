725f0ff126a3904ed3065cbba21f11a482fd1808
Lőrinc Róbert <lorincrobi5@gmail.com>
2021-05-28 17:34:01 +0200

    Első feladat

--- workproject/src/App.js
+++ workproject/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+
+  const [activity, setActivity] = useState("");
+
+  const getRandomActivity = () => {
+    Axios.get("http://www.boredapi.com/api/activity/").then((response) => {
+      console.log(response);
+      setActivity("Activity:\n "  + response.data.activity + ", Type:\n " + response.data.type + ", Participants:\n " + response.data.participants);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getRandomActivity}>Nyomd meg</button><br></br>
+      {activity}
     </div>
   );
 }
